d2a9b33d687bd3b7f88403f02450a566502c2b9e
MrMax01 <massimo.mauro00@gmail.com>
2023-06-19 16:49:53 +0200

    little fix

--- menu.js
+++ menu.js
@@ -9,17 +9,30 @@ $(document).ready(function() {
         $(".js-softDrink-list").slideToggle(900);
     });
 
+
+    /* BEER */
+    $(".js-beer-button").click(function(){
+        $(".js-beer-list").slideToggle(900);
+    });
+
+    /* WINE */
+    $(".js-wine-button").click(function(){
+        $(".js-wine-list").slideToggle(900);
+    });
+
     /* DRINKS */
     $(".js-drink-button").click(function(){
         $(".js-drink-list").slideToggle(900);
     });
+
+
 });
 
   
 function hundleArrow(down, arrowUp){
     const selector = down.slice(1);
     
-    
+
     const flag = document.querySelector(down);
     if(flag){
         document.querySelector(arrowUp).innerHTML = ' <ion-icon name="chevron-up-outline"></ion-icon>';
