2bb7542d45b829b1c0e200fd49fe2e1290d51ad2
MrMax01 <massimo.mauro00@gmail.com>
2023-06-21 23:44:50 +0200

    some comment

--- menu.js
+++ menu.js
@@ -30,12 +30,26 @@ fetch('menu.json')
     // Utilizza l'oggetto JSON ottenuto
     //console.log(data);
     data.forEach( element => {
+
+        /*
+        data è un array di oggetti = {
+            id,
+            titile: [],
+            content: [],
+        }
+        */
         const section = document.getElementById(element.id);
 
         element.title.forEach((value, num)=>{
-             const html = `<h4>${value}</h4>
+            /*
+            -salvo l'html che inserirò nel div id = element.id
+            -  ogni titolo  verrà accostato in html con ogni content della medesima posizione
+            - se il content associato al titolo non c'è,  verrà messa una stringa vuota
+            */
+            const html = `<h4>${value}</h4>
             <p>${element.content[num] || ''}</p>
             <hr>`;
+
             section.innerHTML += html;  
         });
     });
