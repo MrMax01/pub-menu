d297e24c0e09a364f2b957802efeb12c87d513e1
MrMax01 <massimo.mauro00@gmail.com>
2023-06-21 23:28:04 +0200

    update json file
    menu.json contain the menu

--- menu.js
+++ menu.js
@@ -19,3 +19,28 @@ function hundleArrow(down, arrowUp){
         document.querySelector(arrowUp).innerHTML = '<ion-icon name="chevron-down-outline" class='+selector+'></ion-icon>';
     }
 }
+
+
+
+
+/*Carico il menu dal file json*/
+fetch('menu.json')
+  .then(response => response.json())
+  .then(data => {
+    // Utilizza l'oggetto JSON ottenuto
+    //console.log(data);
+    data.forEach( element => {
+        const section = document.getElementById(element.id);
+
+        element.title.forEach((value, num)=>{
+             const html = `<h4>${value}</h4>
+            <p>${element.content[num] || ''}</p>
+            <hr>`;
+            section.innerHTML += html;  
+        });
+    });
+
+  })
+  .catch(error => {
+    console.error('Si è verificato un errore:', error);
+  });
